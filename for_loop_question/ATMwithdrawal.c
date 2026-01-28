cf0f6906f036ee8431b1592b794026aa316dc36b
Gokulnath <gokulj172@gmail.com>
2026-01-29 04:44:41 +0530

    Add files via upload
    ATM withdrawal solved on c

---
+++ for_loop_question/ATMwithdrawal.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int withdrawal,amount;
+    scanf("%d",&withdrawal);
+    int sum=0;
+    
+    for(int i=0;i<withdrawal;i++){
+        scanf("%d",&amount);
+        sum+=amount;
+    }
+    if(sum<10000){
+        printf("Approved");
+    }
+    else{
+        printf("Limit Exceeded");
+    }
+
+}
