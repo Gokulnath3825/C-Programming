b4c7caf9c9f0f2e286690613619b25f5d993f05b
Gokulnath <gokulj172@gmail.com>
2026-02-03 06:50:04 +0530

    Add files via upload
    solved

---
+++ Array_!D/problem19.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int count=0;
+    for(int i=0;i<n;i++){
+        int duplicate=0;
+        for(int j=0;j<i;j++){
+            if(arr[i]==arr[j]){
+                duplicate=1;
+            }
+        }
+        if(duplicate==0){
+            count++;
+        }
+        
+    }
+    printf("%d",count);
+}
