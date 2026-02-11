0f5a1649dc41d94f9baaef9343360f455f600714
Gokulnath <gokulj172@gmail.com>
2026-02-11 10:39:42 +0530

    Add files via upload
    Solved  11 problem

---
+++ Function/example/problem11.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void digit(int n){
+    int count=0;
+    for(int i=n;i>0;i=i/10){
+        count+=1;
+    }
+    printf("%d ",count);
+}
+
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    digit(n);
+}
