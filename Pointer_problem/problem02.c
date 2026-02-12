bcc4671e0b47ade5b4eaf84010b1872b1ce00f3b
Gokulnath <gokulj172@gmail.com>
2026-02-12 15:31:06 +0530

    Implement max function to find maximum in array
    Refactored code to find the maximum value in an array using pointers.

--- Pointer_problem/problem02.c
+++ Pointer_problem/problem02.c
@@ -1,21 +1,24 @@
-#include <stdio.h>
-int findMax(int a, int b) {
-    if (a >= b){
-        return a;
+#include<stdio.h>
+void max(int n,int arr[]){
+    int *p=arr;
+    int max=0;
+    for(int i=0;i<n;i++){
+        int max1=*(p+i);
+        if(max1>max ){
+            max=*(p+i);
+        }
     }
-    else{
-        return b;
-    }
+    printf("%d",max);
+
 }
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    max(n,arr);
 
 
-int main() {
-    int A, B;
-    scanf("%d %d", &A, &B);
-    printf("%d", findMax(A, B));
-
-
-
-    
-    return 0;
 }
