d07a488e90dd69e87a47c72f482f91721ba0bb49
Gokulnath <gokulj172@gmail.com>
2026-03-19 18:18:45 +0530

    Remove spaces in condition check for multiples

--- 08CheckMulOn_3and_7.c
+++ 08CheckMulOn_3and_7.c
@@ -2,7 +2,7 @@
 int main() {
     int num;
     scanf("%d", &num);
-    if(num % 3 == 0 && num % 7 == 0)
+    if(num%3 == 0 && num%7 == 0)
         printf("Multiple of both 3 and 7");
     else
         printf("Not a multiple of both");
