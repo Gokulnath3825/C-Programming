5806c75e8ff36e928a70359cf8569cffe45cc64d
Gokulnath <gokulj172@gmail.com>
2026-09-19 22:53:11 +0530

    Update problem11.c

--- Array_!D/problem11.c
+++ Array_!D/problem11.c
@@ -1,18 +1,26 @@
 #include<stdio.h>
+
 int main(){
+
     int n;
+
     scanf("%d",&n);
+
     int arr[n];
+
     int total=0;
-    int leftSum=0;//1 4 5
+
+    int leftSum=0;
+
     int count=0;
     
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
         total+=arr[i];//13
     }
+
     for(int j=0;j<n;j++){
-        total=total-arr[j]; //13-1=12 12-3=9 9-5=4 4-2=2 2-2==0
+        total=total-arr[j];
         if(leftSum==total){ //0==12 1==9 4==4 9==3  11==2 13==2
             printf("%d",j);
             count=1;
