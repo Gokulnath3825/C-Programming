a043e23712fdeb674980f0fe45f7a6b3037b21ef
Gokulnath <gokulj172@gmail.com>
2026-09-19 22:52:02 +0530

    Update problem01.c

--- Array_!D/problem01.c
+++ Array_!D/problem01.c
@@ -1,20 +1,27 @@
 #include<stdio.h>
+
 int main(){
+
     int size;
+
     scanf("%d",&size);
+
     int arr[size];
 
     for(int i=0;i<size;i++){
+
         scanf("%d",&arr[i]);
+
     }
     
     int max=arr[0];
+
     int secondLargest=0;
  
-    for(int j=0;j<size;j++){  //.   F.         T          F.        
-        if(arr[j]>max){     //  4000 >4000.  4000>9000.  1000>9000.  7000>9000
-            secondLargest=max;// 9000
-            max=arr[j];    // 9000
+    for(int j=0;j<size;j++){
+        if(arr[j]>max) {
+            secondLargest=max;
+            max=arr[j];    
         }
         else if(arr[j]<max && arr[j]>secondLargest){  // 4000<4000. &&. 4000>0.  1000<9000 && 1000>9000
             secondLargest=arr[j];//           F F
