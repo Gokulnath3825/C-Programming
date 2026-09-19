ca3b75c83da29827a1fa65130ac69f4ea285cd3f
Gokulnath <gokulj172@gmail.com>
2026-09-19 22:53:38 +0530

    Update problem12.c

--- Array_!D/problem12.c
+++ Array_!D/problem12.c
@@ -1,17 +1,24 @@
 #include<stdio.h>
 int main(){
+
     int n;
+
     scanf("%d",&n);
+
     int arr[n];
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
 
     int target,count=0,count1=0;
+
     scanf("%d",&target);
     
+
     for(int j=0;j<n;j++){
+
         for(int k=j;k<n;k++){
+
             int total=arr[j]+arr[k];
 
             if(count!=0 && count1==1){
