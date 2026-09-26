136f752ee1ca27cf032f5f90aa89affa6f6d8c57
Gokulnath <gokulj172@gmail.com>
2026-09-26 23:40:49 +0530

    Update 04FindGreaterTwoNum.c

--- 04FindGreaterTwoNum.c
+++ 04FindGreaterTwoNum.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 int main() {
+
     int a, b;
+
     scanf("%d %d", &a, &b);
+
     if(a > b)
         printf("%d is greater", a);
+
     else if(b > a)
         printf("%d is greater", b);
 
     else
+
         printf("Both are equal");
+
     return 0;
 }
